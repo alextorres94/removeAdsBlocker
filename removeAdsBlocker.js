0827903293515bec95d99a87730b4b19a28c2d99
alextorres94 <torres.erades.alex@gmail.com>
2019-09-13 20:04:37 +0200

    Added the ads blocker

---
+++ removeAdsBlocker.js
@@ -0,0 +1,24 @@
+setTimeout(() => removeAdsBlocker(), 5000);
+
+const BLOCKER_IDENTIFIER = '.fc-ab-root'
+const ARTICLE_IDENTIFIER = '.salida_articulo'
+
+function getIdentifier(id) {
+  return document.querySelector(id)
+}
+
+function removeBlocker(blocker, article) {
+  blocker.style.display = 'none'
+  article.style.overflow = 'initial'
+}
+
+function removeAdsBlocker() {
+  const blocker = getIdentifier(BLOCKER_IDENTIFIER)
+  const article = getIdentifier(ARTICLE_IDENTIFIER)
+  const isBlocking = blocker && article
+
+  if (!isBlocking) { return }
+  alert('Removing the blocker...')
+
+  removeBlocker(blocker, article)
+}
